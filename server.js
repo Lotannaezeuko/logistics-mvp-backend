fada2832968f60742c08120bd1be3e711c21d55c
Lotanna Ezeuko <119595586+Lotannaezeuko@users.noreply.github.com>
2025-06-03 14:50:24 +0100

    setup the database connection and tested it. It works

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const pool = require("./config/db");
+require('dotenv').config();
+const cors = require("cors");
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ success: true, time: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
